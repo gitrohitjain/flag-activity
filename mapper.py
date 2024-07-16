939abb76543a62c6be64f9efe80a21c9b3cefa26
Rohit Jain <rohitrbk49@yahoo.co.in>
2024-07-16 12:53:59 +0800

    push code

---
+++ mapper.py
@@ -0,0 +1,155 @@
+from ultralytics import YOLO
+import yaml
+from utils import parts2xy, is_sitting_or_standing, is_person_in_restricted_area
+import cv2
+import os
+
+CONFIG_FILE = 'config.yaml'
+configs = yaml.load(open(CONFIG_FILE), Loader=yaml.FullLoader)
+print(configs)
+
+MODEL_CONFIG = configs['yolo']
+
+model_type =  MODEL_CONFIG['model_type']
+det_conf = MODEL_CONFIG['conf']
+verbose = MODEL_CONFIG['verbose']
+max_det = MODEL_CONFIG['max_det']
+persist = MODEL_CONFIG['persist']
+
+video_path = configs['video_path']
+
+restricted_area = configs['restricted_area']
+
+save_folder = configs['save_folder']
+save_frames = False
+if len(save_folder) > 1:
+    os.makedirs(save_folder, exist_ok=True)
+    if len(os.listdir(save_folder)) > 0:
+        os.system(f"rm -rf {save_folder}/*")
+    save_frames = True
+
+
+x1, y1, x2, y2 = restricted_area['x1'], restricted_area['y1'], restricted_area['x2'], restricted_area['y2']
+restricted_area_coords =(x1, y1, x2, y2)
+ok_color = configs['colors']['ok']
+danger_color = configs['colors']['danger']
+alarm_config = configs['alarm']
+standing_treshold_in_secs = alarm_config['alarm_time_standing']
+sitting_treshold_in_secs = alarm_config['alarm_time_sitting']
+
+
+class Tracker:
+    def __init__(self, model_type=model_type):
+        self.model = YOLO(model_type)
+        self.ids_time = {}
+        self.ids_time_total = {}
+
+    def get_details(self):
+        bboxes_ids = self.results.boxes.id.cpu().numpy()
+        if len(bboxes_ids) == 0:
+            return None
+        bboxes_xyxy = self.results.boxes.xyxy.cpu().numpy().astype(int)
+        bboxes_conf = self.results.boxes.conf.cpu().numpy()
+        kps = self.results.keypoints.xy.cpu().numpy()
+        kps = kps.astype(int)
+        kps_conf = self.results.keypoints.conf.cpu().numpy()
+
+        return {
+            "bboxes_xyxy": bboxes_xyxy,
+            "bboxes_ids": bboxes_ids,
+            "bboxes_conf": bboxes_conf,
+            "kps": kps,
+            "kps_conf": kps_conf
+        }
+
+    def run_ai(self):
+        cap = cv2.VideoCapture(video_path)
+        count = 0
+        while cap.isOpened():
+            success, frame = cap.read()
+            fps = cap.get(cv2.CAP_PROP_FPS)
+            standing_treshold_in_fps = standing_treshold_in_secs * fps
+            sitting_treshold_in_fps = sitting_treshold_in_secs * fps
+
+
+            if success:
+                
+                self.results = self.model.track(frame, persist=persist, verbose=verbose, conf=det_conf)[0]
+
+                try:
+                    bbox_details = self.get_details()
+                    bboxes_xyxy, bboxes_ids, bboxes_conf, kps, kps_conf = bbox_details["bboxes_xyxy"], bbox_details["bboxes_ids"], bbox_details["bboxes_conf"], bbox_details["kps"], bbox_details["kps_conf"]
+                except Exception as e:
+                    print(e)
+                    continue
+                if not len(bboxes_ids) > 0:
+                    continue
+                img_frame = self.results.orig_img
+
+                cv2.rectangle(img_frame, \
+                            (restricted_area_coords[0], restricted_area_coords[1]), \
+                            (restricted_area_coords[2], restricted_area_coords[3]), \
+                            (255,0,225), 2)
+
+                for i in range(len(bboxes_ids)):
+                    id_ = round(bboxes_ids[i])
+                    if id_ in self.ids_time_total:
+                        self.ids_time_total[id_] += 1
+                    else:
+                        self.ids_time_total[id_] = 0
+
+                    bbox = bboxes_xyxy[i]
+                    bbox_conf = bboxes_conf[i]
+                    kps_ = kps[i]
+                    kps_conf_ = kps_conf[i]
+                    current_pose = is_sitting_or_standing(parts2xy(kps_))
+                    under_restricted_area = is_person_in_restricted_area(kps_, restricted_area_coords)
+                    is_danger = False
+                    color = ok_color
+                    if under_restricted_area:
+                        if id_ in self.ids_time:
+                           self.ids_time[id_][current_pose] += 1
+                        else:
+                            self.ids_time[id_] = {
+                                "standing": 0,
+                                "sitting": 0,
+                                "unknown": 0
+                            }
+                    else:
+                        if id_ in self.ids_time:
+                            self.ids_time[id_][current_pose] = 0
+
+                    if self.ids_time.get(id_):
+                        if self.ids_time[id_]['standing'] > standing_treshold_in_fps:
+                            is_danger = True
+                            color = danger_color
+
+                        if self.ids_time[id_]['sitting'] > sitting_treshold_in_fps:
+                            is_danger = True
+                            color = danger_color
+
+                    cv2.rectangle(img_frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), color, 2)
+                    cv2.rectangle(img_frame, (bbox[0], bbox[1]-15), (bbox[2], bbox[1]+10), (0, 0, 0), -1)
+                    cv2.putText(img_frame, f"{id_}: {current_pose}", ((bbox[0]+bbox[2])//2 - 35, bbox[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+                sn = str(count)
+                sn = sn.zfill(6)
+                if is_danger:
+                    print(f"danger detected in frame {sn}")
+                
+                offset = 25
+                for ix, (id_, time_) in enumerate(self.ids_time_total.items()):
+                    time_ = round(time_ / fps, 2)
+                    cv2.putText(img_frame, f"{id_} : {time_} secs", (10, offset + ix*20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255) , 2)
+                
+                cv2.imwrite(f"{save_folder}/{sn}.jpg", img_frame)
+                count += 1
+            
+
+    
+
+if __name__ == "__main__":
+    tracker = Tracker()
+    tracker.run_ai()
+    
+
---
+++ utils.py
@@ -0,0 +1,71 @@
+import math
+
+def parts2xy(kps_):
+    return {
+        "nose": kps_[0],
+        "left_eye": kps_[1],
+        "right_eye": kps_[2],
+        "left_ear": kps_[3],
+        "right_ear": kps_[4],
+        "left_shoulder": kps_[5],
+        "right_shoulder": kps_[6],
+        "left_elbow": kps_[7],
+        "right_elbow": kps_[8],
+        "left_wrist": kps_[9],
+        "right_wrist": kps_[10],
+        "left_hip": kps_[11],
+        "right_hip": kps_[12],
+        "left_knee": kps_[13],
+        "right_knee": kps_[14],
+        "left_ankle": kps_[15],
+        "right_ankle": kps_[16]
+    }
+
+def calculate_angle(a, b, c):
+    ab = (a[0] - b[0], a[1] - b[1])
+    bc = (c[0] - b[0], c[1] - b[1])
+    dot_product = ab[0] * bc[0] + ab[1] * bc[1]
+    magnitude_ab = math.sqrt(ab[0]**2 + ab[1]**2)
+    magnitude_bc = math.sqrt(bc[0]**2 + bc[1]**2)
+    cos_angle = dot_product / (magnitude_ab * magnitude_bc)
+    cos_angle = max(-1, min(1, cos_angle))
+    angle = math.acos(cos_angle)
+    return int(math.degrees(angle))
+
+def is_sitting_or_standing(keypoints):
+    left_hip = keypoints["left_hip"]
+    right_hip = keypoints["right_hip"]
+    left_knee = keypoints["left_knee"]
+    right_knee = keypoints["right_knee"]
+    left_ankle = keypoints["left_ankle"]
+    right_ankle = keypoints["right_ankle"]
+
+    hip_to_ankle_left = abs(left_hip[1] - left_ankle[1])
+    hip_to_ankle_right = abs(right_hip[1] - right_ankle[1])
+    knee_angle_left = calculate_angle(left_hip, left_knee, left_ankle)
+    knee_angle_right = calculate_angle(right_hip, right_knee, right_ankle)
+
+    vertical_threshold = 100  
+    angle_threshold = 150  
+    sitting_angle = 140
+
+    if hip_to_ankle_left > vertical_threshold and hip_to_ankle_right > vertical_threshold:
+        if knee_angle_left > angle_threshold and knee_angle_right > angle_threshold:
+            return "standing" #confident
+    else:
+        if knee_angle_left < sitting_angle and knee_angle_right < sitting_angle:
+            return "sitting" #confident
+    
+    return "standing" #or sitting can be configured based on application
+
+
+def is_person_in_restricted_area(kps_, restricted_area_rect_coords):
+    for k in kps_:
+        if k[0] > restricted_area_rect_coords[0] \
+            and k[0] < restricted_area_rect_coords[2] \
+            and k[1] > restricted_area_rect_coords[1] \
+            and k[1] < restricted_area_rect_coords[3]:
+            
+            return True
+        
+    return False
